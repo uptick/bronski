e613631837c053bb140cdabcc462f03bfda7cd29
Curtis Maloney <curtis@tinbrain.net>
2020-03-16 15:49:53 +1100

    Add missed_jobs helper

--- src/bronski/models.py
+++ src/bronski/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 
 # TODO : support "universal" JSON field?
 from django.contrib.postgres.fields import JSONField
@@ -14,6 +15,27 @@ def cron_validator(value):
         raise ValidationError("Invalid crontab format")
 
 
+class CrontabBaseManager(models.Manager):
+
+    def missed_jobs(self):
+        """
+        yields jobs whose last run is longer ago than their crontab frequency.
+        """
+        now = datetime.now()
+
+        with transaction.atomic():
+            qset = (
+                self.get_queryset()
+                .filter(is_enabled=True)
+                # Use skip_locked to catch any new tasks added, and avoid DB error
+                .select_for_update(skip_locked=True)
+            )
+            for job in qset:
+                next_run = croniter(job.crontab, job.last_run).get_next(datetime)
+                if next_run < now:
+                    yield job
+
+
 class CrontabBase(models.Model):
     '''
     Abstract base model for custom Bronski crontab model.
@@ -23,6 +45,8 @@ class CrontabBase(models.Model):
     kwargs = JSONField(default=dict, blank=True)
     is_enabled = models.BooleanField(default=True)
     last_run = models.DateTimeField(default=now)
+
+    objects = CrontabBaseManager()
 
     class Meta:
         abstract = True
